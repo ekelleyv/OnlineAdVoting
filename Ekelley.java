6fd488f8d9175010d5986bfae2ec4fd2e41111c7
Ed Kelley <ekelley@princeton.edu>
2012-11-25 23:34:47 -0500

    Finding utils

--- Ekelley.java
+++ Ekelley.java
@@ -43,39 +43,76 @@ public class Ekelley
             Matcher m = roundpattern.matcher(s);
             m.matches();
 
-            int round = Integer.parseInt(m.group(1));
             String[] sbids = m.group(2).split(",");
             String[] sslots = m.group(3).split(",");
             String[] spayments = m.group(4).split(",");
             String[] sclicks = m.group(5).split(",");
             for (int i = 0; i < nagents-1; ++i) {
-                bids[round][i] = Integer.parseInt(sbids[i]);
-                slots[round][i] = Integer.parseInt(sslots[i]);
-                payments[round][i] = Integer.parseInt(spayments[i]);
-                clicks[round][i] = Integer.parseInt(sclicks[i]);
+                bids[round-1][i] = Integer.parseInt(sbids[i]);
+                slots[round-1][i] = Integer.parseInt(sslots[i]);
+                payments[round-1][i] = Integer.parseInt(spayments[i]);
+                clicks[round-1][i] = Integer.parseInt(sclicks[i]);
             }
             bids[round][nagents-1] = Integer.parseInt(sbids[nagents-1]);
         }
 
-        int playRound(String round_info) {
-                int bid_value;
+        int mySlot(int round_number) {
+                int slot_number = -1; //didn't get one
+                for (int i = 0; i < (nagents-1); i++) {
+                        if (slots[round_number][i] == myId) {
+                                slot_number = i;
+                                return slot_number;
+                        }
+                }
+                return slot_number;
+        }
 
+        int playRound(String round_info) {
+                int bid_value = 0;
                 if (round == 0) {
-                        bid_value = myVal;
+                        //bid_value = myVal;
+                        bid_value = 84;
+                        remaining_budget = budget;
                 }
                 else {
                         parseRound(round_info);
-                        bid_value = 20;
+                        remaining_budget -= payments[round-1][myId];
+                        int last_slot = mySlot(round-1);
+                        int last_bid = bids[round-1][myId];
+                        System.out.println("Last slot = " + last_slot + " Last bid = " + last_bid);
+
+                        //Estimated value of each slot based on last turn
+                        int[] estimated_util = new int[nagents-1];
+                        int max_util_index = nagents-1;
+                        int max_util = 0;
+
+                        // System.out.println("i" + " : " + "clickrate" + " - " + "myVal" + " - " + "bids" + " - " + "payments" + " - " + "this_util");
+
+                        for (int i = 0; i < (nagents-1); i++) {
+                                int clickrate = (int)clickRate(round, i);
+                                int this_util = clickrate*(myVal-payments[round-1][i]);
+                                System.out.println(i + " : " + clickrate + " - " + myVal + " - " + bids[round-1][i] + " - " + payments[round-1][i] + " - " + this_util);
+                                estimated_util[i] = this_util;
+                                if (this_util > max_util) {
+                                        max_util_index = i;
+                                        max_util = this_util;
+                                }
+                        }
+
+
+                        System.out.println("Best index is " + max_util_index);
+                        System.out.println("With a max_util of " + max_util);
+                        
+                        
                 }
 
                 round++;
-                remaining_budget -= bid_value;
                 return bid_value;
         }
 
-        double clickRate(int round, int slot) {
-                int c1 = (int)Math.round(30*Math.cos((2*Math.PI*round)/numrounds) + 50);
-                return (int)Math.pow(0.75, round-1)*c1;
+        int clickRate(int round_number, int slot) {
+                int c0 = (int)Math.round(30*Math.cos((2*Math.PI*round_number)/numrounds) + 50);
+                return (int)(Math.pow(0.75, slot)*c0);
         }
 
         public static void main(String[] args) throws java.io.IOException
